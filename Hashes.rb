43d8a3fd80a4f5ef2c1d0d73e54a87d5284b8d64
Harsha <96243890+Rage-Fox@users.noreply.github.com>
2022-06-08 18:50:07 +0530

    Create Hashes.rb

---
+++ Hashes.rb
@@ -0,0 +1,11 @@
+states = {
+  "Pennslvania" => "PA",
+  "New York" => "NY",
+  "Oregon" => "OR",
+  :Florida => "FL", #: is same as ""
+  1 => "One" #we can alo give numbers to keys
+} #keys and values just like dictionaries or hashes in python
+puts states
+puts states["New York"]
+puts states[:Florida]
+#ruby Ruby.rb
